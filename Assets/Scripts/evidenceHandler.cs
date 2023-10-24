c3619634236c6b5ed2692acda7587e26bc5d8bba
citrusUprising <62976444+citrusUprising@users.noreply.github.com>
2023-10-23 17:50:02 -0700

    added code structure for evidence

---
+++ Assets/Scripts/evidenceHandler.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class evidenceHandler : MonoBehaviour
+{
+    public GameObject diplayButton;
+    public GameObject nameObject;
+    public GameObject textWall;
+    public GameObject tab;
+    public Color highlight;
+    public Color standard;
+    public string name;
+    private bool isActive;
+    public int questionsLeft = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void enable (){
+        //displayButton.enable;
+        nameObject.GetComponent<TextMeshProUGUI>().text = name;
+    }
+
+    public void toggleText(){
+        //if(textWall.isActive) textWall.disabl
+        isActive = !isActive;
+        if(isActive){
+            tab.GetComponent<Image>().color = highlight;
+        }else{
+            tab.GetComponent<Image>().color = standard;
+        }
+    }
+}
