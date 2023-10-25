e1ec94618f551ae7390fd02edff05878bf04e0f0
ManojSara <msara@ucsc.edu>
2023-10-25 14:22:22 -0700

    basic system

---
+++ Assets/Scripts/ProgressText.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngine.InputSystem;
+
+public class ProgressText : MonoBehaviour
+{
+    public InkManager inkManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inkManager = FindObjectOfType<InkManager>();
+
+        if (inkManager == null)
+        {
+            Debug.Log("Ink Manager not found");
+        }
+
+        Debug.Log("Turned on");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("OnClick()");
+        inkManager.DisplayNextLine();
+    }
+}
