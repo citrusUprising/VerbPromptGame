fc6caa82a18ff332874f6cdf944dbbce946f6515
ManojSara <msara@ucsc.edu>
2023-10-26 16:46:21 -0700

    merge?

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Ink.Runtime;
+
+public class DialoguePassIn : MonoBehaviour
+{
+    // public 
+    public Sprite sprite;
+
+    // public Character(newStory, newSprite)
+    // {
+    //     story = newStory;
+    //     sprite = newSprite;
+    // }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
